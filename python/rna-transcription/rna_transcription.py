ae2b8afed7f689d06fca909d9d6ca8993ae328c3
Tim Condit <tcondit@gmail.com>
2016-12-28 13:54:48 -0800

    remove README

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,24 +1,3 @@
-'''
-Write a program that, given a DNA strand, returns its RNA complement (per RNA
-transcription).
-
-Both DNA and RNA strands are a sequence of nucleotides.
-
-The four nucleotides found in DNA are adenine (**A**), cytosine (**C**),
-guanine (**G**) and thymine (**T**).
-
-The four nucleotides found in RNA are adenine (**A**), cytosine (**C**),
-guanine (**G**) and uracil (**U**).
-
-Given a DNA strand, its transcribed RNA strand is formed by replacing each
-nucleotide with its complement:
-
-* `G` -> `C`
-* `C` -> `G`
-* `T` -> `A`
-* `A` -> `U`
-'''
-
 # NB: May raise a KeyError, will not handle it
 def dna_nucleotide_to_rna(dna):
     dna_rna_dict = { 'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U' }
