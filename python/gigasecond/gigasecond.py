fe1eca0af2d00297f847b7a0228d9a0edd99b892
Tim Condit <tcondit@gmail.com>
2016-12-28 16:39:22 -0800

    take formatting idea from another's solution

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,6 +1,4 @@
 import datetime
 
-GIGASECOND = 1000000000
-
 def add_gigasecond(datetime_obj):
-    return datetime_obj + datetime.timedelta(seconds=GIGASECOND)
+    return datetime_obj + datetime.timedelta(seconds=10**9)
