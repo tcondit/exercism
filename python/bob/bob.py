a1fd5d53706101c80ccfc05ae234d4d7e97f14ef
Tim Condit <tcondit@gmail.com>
2016-12-28 17:45:58 -0800

    fix tests

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,23 +1,11 @@
-import string
-
 def hey(what):
     what = what.strip()
-    what_upper = what.strip().upper()
 
-    print(what, what_upper)
-
-    if what == '':
-        return 'Fine. Be that way!' 
-
-    # Both assertions currently return 'Whoa, chill out!'
-    #
-    # self.assertEqual('Whatever.', bob.hey('1, 2, 3')) -> fail
-    # self.assertEqual('Sure.', bob.hey('4?'))          -> fail
-
-    elif what == what_upper:
+    if what.isupper():
         return 'Whoa, chill out!'
-
     elif what.endswith('?'):
         return 'Sure.'
+    elif what == '':
+        return 'Fine. Be that way!'
     else:
         return 'Whatever.'
